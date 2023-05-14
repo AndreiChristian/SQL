66e4b1047b1051a10841048606d1d2a777dd0b77
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-05-14 12:14:18 +0200

    first steps

---
+++ 1.Definition/01-create-database.sql
@@ -0,0 +1,6 @@
+-- the name of the db should have no white spaces, and it should not clash with
+-- reserved keywoards such as create etc. If we want to use such a name, we should 
+-- wrap with up ""(most common, including postgre) or ``(for mysql) 
+CREATE DATABASE talently;
+
+
